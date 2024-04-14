97757d2b1944a855ce6b4bace980d7c8cec78d2b
magcipie2007 <magicpie2007@gmail.com>
2024-04-15 08:45:59 +0900

    Initial Commit

---
+++ MotionUtil/MotionUtilApp.swift
@@ -0,0 +1,32 @@
+//
+//  MotionUtilApp.swift
+//  MotionUtil
+//
+//  Created by hiroyuki shoji on 2024/04/15.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct MotionUtilApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
