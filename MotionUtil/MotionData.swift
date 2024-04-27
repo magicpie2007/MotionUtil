33331249ebe35c3be981c5b638be114fccb4cef3
magcipie2007 <magicpie2007@gmail.com>
2024-04-27 12:50:42 +0900

    Change folder/file structure about CSV log

--- MotionUtil/MotionData.swift
+++ MotionUtil/MotionData.swift
@@ -14,32 +14,35 @@ class MotionData: ObservableObject {
     private let logger = Logger(subsystem: "com.hsworks.MotionUtil", category: "MotionData")
     private let motionManager = CMMotionManager()
     private let accDataCsvWriter = CsvWriter()
-    private let folderName: String
-    private var activity: String = "Stay"
+    private let dateFolderName: String
+    private var csvStoreFolderName: String = ""
     @Published var accX: Double = 0.0
     @Published var accY: Double = 0.0
     @Published var accZ: Double = 0.0
 
     init() {
-        self.folderName = { () -> String in
+        self.logger.info("init() is called")
+        self.dateFolderName = { () -> String in
             // Define folder name
             let dateFormatter = DateFormatter()
             dateFormatter.dateFormat = "yyyyMMdd"
             return dateFormatter.string(from: Date()) + "-" + "CSV"
         }()
-        createFolderInDocumentDirectory(folderName: folderName)
+        createFolderInDocumentDirectory(folderName: self.dateFolderName)
     }
 
     func setActivity(activity: String) {
-        self.activity = activity
+        self.logger.info("setActivity() is called: activity=\(activity)")
+        self.csvStoreFolderName = self.dateFolderName + "/" + activity
+        createFolderInDocumentDirectory(folderName: self.csvStoreFolderName)
     }
 
     func start() {
         self.logger.info("start() is called")
-        self.createCsv(activity: self.activity)
-        if motionManager.isAccelerometerAvailable {
-            motionManager.accelerometerUpdateInterval = 0.5
-            motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: { data, error in
+        self.createCsv()
+        if self.motionManager.isAccelerometerAvailable {
+            self.motionManager.accelerometerUpdateInterval = 0.2
+            self.motionManager.startAccelerometerUpdates(to: OperationQueue.current!, withHandler: { data, error in
                 guard let data = data, error == nil else { return }
                 self.accX = data.acceleration.x
                 self.accY = data.acceleration.y
@@ -52,23 +55,22 @@ class MotionData: ObservableObject {
 
     func stop() {
         self.logger.info("stop() is called")
-        if motionManager.isAccelerometerActive {
-            motionManager.stopAccelerometerUpdates()
+        if self.motionManager.isAccelerometerActive {
+            self.motionManager.stopAccelerometerUpdates()
         }
-        accDataCsvWriter.closeCsv()
+        self.accDataCsvWriter.closeCsv()
     }
 
-    private func createCsv(activity: String) {
+    private func createCsv() {
         self.logger.info("createCsv() is called")
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyyMMdd-HHmmss"
-        let fileName = "AccData-" + activity + "-" + dateFormatter.string(from: Date()) + ".csv"
+        let fileName = "AccData-" + dateFormatter.string(from: Date()) + ".csv"
 
-        let folderPath = NSHomeDirectory() + "/Documents/" + folderName
-
-        let accDataCsvPath = folderPath + "/" + fileName
+        let accDataCsvPath = NSHomeDirectory() + "/Documents/" + self.csvStoreFolderName + "/" + fileName
+        self.logger.info("createCsv(): CSV path=\(accDataCsvPath)")
         let accDataCsvHeader = "timestamp,accX,accY,accZ\r\n"
-        accDataCsvWriter.openCsv(filePath: accDataCsvPath,
-                                 header: accDataCsvHeader)
+        self.accDataCsvWriter.openCsv(filePath: accDataCsvPath,
+                                      header: accDataCsvHeader)
     }
 }
